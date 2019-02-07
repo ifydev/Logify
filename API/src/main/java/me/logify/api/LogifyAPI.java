1f19045eeddbe1d5c45b405d673c5b506331897d
Innectic <innectic@gmail.com>
2019-02-07 12:54:45 -0800

    Initial commit

---
+++ API/src/main/java/me/logify/api/LogifyAPI.java
@@ -0,0 +1,12 @@
+package me.logify.api;
+
+/**
+ * @author Innectic
+ * @since 02/07/2019
+ */
+public class LogifyAPI {
+
+    public void initialize() {
+
+    }
+}
---
+++ Spigot/src/main/java/me/logify/spigot/LogifyMain.java
@@ -0,0 +1,20 @@
+package me.logify.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Innectic
+ * @since 02/07/2019
+ */
+public class LogifyMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
